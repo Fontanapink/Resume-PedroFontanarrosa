02997be2bd2ea454635d7378ea6f3e17a460373a
Pedro Fontanarrosa <pfontanarrosa@gmail.com>
2025-03-06 11:02:31 +0000

    updated the generate-pdf.js to a) render the temp-html file to a pdf file and b) pass some -no sandbox flags so that the github actions can run the headless chrome

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,11 +1,12 @@
-// filepath: generate-pdf.js
 const puppeteer = require('puppeteer');
 const path = require('path');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
-    const htmlFilePath = 'file://' + path.resolve('docs/index.html');
+    const htmlFilePath = 'file://' + path.resolve('docs/temp-pdf.html');
     await page.goto(htmlFilePath, { waitUntil: 'networkidle0' });
     await page.pdf({ path: 'docs/cv.pdf', format: 'A4' });
     await browser.close();
